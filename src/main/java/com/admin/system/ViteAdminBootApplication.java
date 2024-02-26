1cbad431bbc1ca18017a3b502dff213d3293944d
wuchuan123 <609833577@qq.com>
2024-02-26 22:38:00 +0800

    init

---
+++ src/main/java/com/admin/system/ViteAdminBootApplication.java
@@ -0,0 +1,12 @@
+package com.admin.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViteAdminBootApplication {
+	public static void main(String[] args) {
+		System.out.print("起来了哦");
+		SpringApplication.run(ViteAdminBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/admin/system/ViteAdminBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViteAdminBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
